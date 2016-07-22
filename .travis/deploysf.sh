3918fddc194f44a854638d96c2be5b1c7e3e6a09
mob41 <puihuenlaw@gmail.com>
2016-07-23 02:13:25 +0800

    Deploy script fixes #4

--- .travis/deploysf.sh
+++ .travis/deploysf.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cp KmbETA-API-$projectversion-$TRAVIS_BRANCH-B$TRAVIS_BUILD_NUMBER.zip KmbETA-API-$projectversion-latest.zip
 ls -l
 scp -v -i ~/.ssh/id_rsa KmbETA-API-$projectversion-latest.zip mob41,kmbeta-api@frs.sourceforge.net:/home/frs/project/k/km/kmbeta-api
