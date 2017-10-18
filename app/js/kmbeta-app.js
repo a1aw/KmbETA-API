2df7e2078da1546fdb651fd02ff66ff7d1cf17c0
mob41 <puihuenlaw@gmail.com>
2017-10-18 16:33:42 +0800

    Add route search

--- app/js/kmbeta-app.js
+++ app/js/kmbeta-app.js
@@ -9,6 +9,11 @@ var currLocUptTimerId;
 var currLocMarker;
 var map;
 var kmbDb;
+var currPos;
+
+var routePaths = [];
+//var routeLines = [];
+//var routeMarkers = [];
 
 function initMap(){
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -28,6 +33,7 @@ function initMap(){
 			  lat: position.coords.latitude,
 			  lng: position.coords.longitude
 			}
+			currPos = pos;
 			
 			currLocMarker = new google.maps.Marker({position: pos, map: map});
 			
@@ -52,7 +58,7 @@ function kmbEtaLoadDb(){
 	$("#loadDbPbText").html("0% Complete");
 	$("#loadDbModal").modal({backdrop: 'static', keyboard: false});
 	Database.prototype.loadProgressHandler = function(p){
-		p = p * 100;
+		p = (p * 100).toFixed(2);
 	    $("#loadDbPb").attr("aria-valuenow", p);
 	    $("#loadDbPb").attr("style", "width: " + p + "%");
 	    $(".loadDbPbText").html(p + "% Complete");
@@ -60,7 +66,129 @@ function kmbEtaLoadDb(){
 	var ajax = kmbDb.loadWebDb();
 	ajax.done(function(){
 	    $("#loadDbModal").modal('hide');
+		listAllRoutesInRange(1);
 	});
+}
+
+function listAllRoutesInRange(range){
+	var rr = findRoutesInRange(range);
+	
+	for (var i = 0; i < rr.length; i++){
+	    var index = findRouteIndex(rr[i].name);
+        if (index != -1){
+			buildRouteLinesAndMarkers(index, 0);
+        } else {
+            console.log("Error: Could not find route index for " + rr[i].name);    
+			alert("Error: Could not find route index for " + rr[i].name + "\nPlease report to GitHub issue tracker!");   
+		}		
+	}
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function buildRouteLinesAndMarkers(routeIndex, boundIndex){
+	var coord = [];
+	
+	var routeStops = kmbDb.db.buses[routeIndex].bounds[boundIndex].stops;
+	
+	for (var i = 0; i < routeStops.length; i++){
+	    coord.push({
+		    lat: parseInt(routeStops.lat),
+            lng: parseInt(routeStops.lng)			
+		});	
+	}
+	
+	var color = getRandomColor();
+	var path = new google.maps.Polyline({
+		path: coord,
+		geodesic: true,
+		strokeColor: color,
+		strokeOpacity: 0.7,
+		strokeWeight: 4
+	});
+	
+	path.setMap(map);
+	routePaths.push(path);
+}
+
+function findRouteIndex(routeName){
+    for (var i = 0; i < kmbDb.db.buses.length; i++){
+	    if (kmbDb.db.buses[i].name === routeName){
+            return i;
+		}		
+	}
+    return -1;	
+}
+
+function findRoutesInRange(range){
+    var sr = findStopsInRange(range);
+	
+    var i;
+	var x;
+	var y;
+    var o = [];
+	
+	var db = kmbDb.db.buses;
+	for (i = 0; i < db.length; i++){
+		var bounds = db[i].bounds;
+		for (x = 0; x < bounds.length; x++){
+		    var stops = bounds[x].stops;
+            for (y = 0; y < stops.length; y++){
+				if (isStopCodeInArray(sr, stops[y].stopcode) &&
+				    !isRouteNameInArray(o, db[i].name)){
+					o.push(db[i]);
+				}
+            }			
+		}
+    }
+	
+    return o;	
+}
+
+function findStopsInRange(range){
+    var db = kmbDb.db.buses;
+    var i;
+	var x;
+	var y;
+	var o = [];
+    for (i = 0; i < db.length; i++){
+		var bounds = db[i].bounds;
+		for (x = 0; x < bounds.length; x++){
+		    var stops = bounds[x].stops;
+            for (y = 0; y < stops.length; y++){
+                var d = distance(currPos.lat, currPos.lng, stops[y].lat, stops[y].lng);
+				if (d <= range && !isStopCodeInArray(o, stops[y].stopcode)){
+					o.push(stops[y]);
+				}
+            }			
+		}
+    }
+	return o;
+}
+
+function isRouteNameInArray(array, name){
+	for (var i = 0; i < array.length; i++){
+	    if (name === array[i].name){
+            return true;
+        }		
+	}
+	return false;
+}
+
+function isStopCodeInArray(array, stopcode){
+	for (var i = 0; i < array.length; i++){
+	    if (stopcode === array[i].stopcode){
+            return true;
+        }		
+	}
+	return false;
 }
 
 function distance(lat1, lon1, lat2, lon2) {
@@ -86,7 +214,7 @@ function uptCurrLocMarker(){
 		  lat: position.coords.latitude,
 		  lng: position.coords.longitude
 		}
-		
+		currPos = pos;
 		currLocMarker.setPosition(pos);
 	}, function(){
 		clearInterval(currLocUptTimerId);
