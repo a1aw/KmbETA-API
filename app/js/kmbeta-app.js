50d05d0fffe9d331f9b6638c0ed4f7c0f9a47cb0
mob41 <puihuenlaw@gmail.com>
2017-10-18 13:17:32 +0800

    Mod html js

--- app/js/kmbeta-app.js
+++ app/js/kmbeta-app.js
@@ -1,19 +1,36 @@
 $(document).ready(function(){
-	$("#waitMapModal").modal({backdrop: 'static', keyboard: false});
+	if (!window.location.protocol.startsWith("https:") && !window.location.protocol.startsWith("file:")){
+		window.location = "https://www.kmbeta.ml/app/";
+	}
 });
 
+var currLocUptTimerId;
+var currLocMarker;
+var map;
+
 function initMap(){
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 22.25, lng: 114.1667},
         zoom: 12
     });
 
 	//$("#waitMapModal").modal('hide');
 	
+	$("#waitMapModal").modal({backdrop: 'static', keyboard: false});
 	if (navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(function(pos){
-			
+		navigator.geolocation.getCurrentPosition(function(position){
 		    $("#waitMapModal").modal('hide');
+			
+			var pos = {
+			  lat: position.coords.latitude,
+			  lng: position.coords.longitude
+			}
+			
+			currLocMarker = new google.maps.Marker({position: pos, map: map});
+			
+			map.setCenter(pos);
+			map.setZoom(16);
+			setInterval(function(){uptCurrLocMarker()}, 1000);
 		}, function(){
 		    $("#waitMapModal").modal('hide');
 	        $("#noMapModal").modal({backdrop: 'static', keyboard: false});
@@ -22,4 +39,19 @@ function initMap(){
 		$("#waitMapModal").modal('hide');
 	    $("#noMapModal").modal({backdrop: 'static', keyboard: false});
 	}
+}
+
+function uptCurrLocMarker(){
+	navigator.geolocation.getCurrentPosition(function(position){
+		var pos = {
+		  lat: position.coords.latitude,
+		  lng: position.coords.longitude
+		}
+		
+		currLocMarker.setPosition(pos);
+	}, function(){
+		clearInterval(currLocUptTimerId);
+		$("#waitMapModal").modal('hide');
+	    $("#noMapModal").modal({backdrop: 'static', keyboard: false});
+	});
 }
